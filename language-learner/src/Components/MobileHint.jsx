242db43d58d1eea5d1034477f0255f87074a7688
github username <github email address>
2025-01-06 10:29:14 -0500

    hint styling

--- language-learner/src/Components/MobileHint.jsx
+++ language-learner/src/Components/MobileHint.jsx
@@ -1,47 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import { useGameState } from "../Contexts/GameStateContext.js";
 import "../Styles/MobileHint.scss";
 
 
 const MobileHint = () => {
-
-    const {setOptions} = useGameState();
+    const [touched, setTouched] = useState(false);
+    const {setOptions, screenSize} = useGameState();
 
     const handleTouchStart = (e) => {
-        setOptions((prevOptions) => ({
-            ...prevOptions, // Spread the existing options
-            characterTypes: {
-              ...prevOptions.characterTypes, // Spread the existing characterTypes
-              romaji: {
-                ...prevOptions.characterTypes.romaji, // Spread the existing romaji object
-                activeTop: true, // Update the specific property
-              },
+      setTouched(true)
+      setOptions((prevOptions) => ({
+          ...prevOptions, // Spread the existing options
+          characterTypes: {
+            ...prevOptions.characterTypes, // Spread the existing characterTypes
+            romaji: {
+              ...prevOptions.characterTypes.romaji, // Spread the existing romaji object
+              activeTop: true, // Update the specific property
             },
-        }));
+          },
+      }));
     };
 
     const handleTouchEnd = (e) => {
-        setOptions((prevOptions) => ({
-            ...prevOptions,
-            characterTypes: {
-              ...prevOptions.characterTypes,
-              romaji: {
-                ...prevOptions.characterTypes.romaji,
-                activeTop: false, // Set back to false when keys are released
-              },
+      setTouched(false)
+      setOptions((prevOptions) => ({
+          ...prevOptions,
+          characterTypes: {
+            ...prevOptions.characterTypes,
+            romaji: {
+              ...prevOptions.characterTypes.romaji,
+              activeTop: false, // Set back to false when keys are released
             },
-        }));
+          },
+      }));
     };
     
-
+    
     return (
         <div className="mobile-hint-container">
+          {(screenSize === 'mobile' || screenSize === 'tablet') && (
             <button 
-                className="mobile-hint-button"
+                className={`${touched && 'active'} mobile-hint-button`}
                 onTouchStart={handleTouchStart}
                 onTouchEnd={handleTouchEnd}>
                 <i class="fa-regular fa-lightbulb"></i>
             </button>
+          )}
         </div>
     );
 
