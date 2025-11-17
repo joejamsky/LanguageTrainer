37eb2f8d86bece0a8880b7ef91dcdc7755d14307
github username <github email address>
2025-11-16 20:38:12 -0500

    mobile hint

--- language-learner/src/Components/MobileHint.jsx
+++ language-learner/src/Components/MobileHint.jsx
@@ -5,47 +5,37 @@ import "../Styles/MobileHint.scss";
 
 const MobileHint = () => {
     const [touched, setTouched] = useState(false);
-    const {setOptions, screenSize} = useGameState();
+    const {setOptions} = useGameState();
 
-    const handleTouchStart = (e) => {
+    const handleTouchStart = () => {
       setTouched(true)
       setOptions((prevOptions) => ({
-          ...prevOptions, // Spread the existing options
-          characterTypes: {
-            ...prevOptions.characterTypes, // Spread the existing characterTypes
-            romaji: {
-              ...prevOptions.characterTypes.romaji, // Spread the existing romaji object
-              activeTop: true, // Update the specific property
-            },
-          },
+          ...prevOptions,
+          hints: true,
       }));
     };
 
-    const handleTouchEnd = (e) => {
+    const handleTouchEnd = () => {
       setTouched(false)
       setOptions((prevOptions) => ({
           ...prevOptions,
-          characterTypes: {
-            ...prevOptions.characterTypes,
-            romaji: {
-              ...prevOptions.characterTypes.romaji,
-              activeTop: false, // Set back to false when keys are released
-            },
-          },
+          hints: false,
       }));
     };
     
     
     return (
         <div className="mobile-hint-container">
-          {(screenSize === 'mobile' || screenSize === 'tablet') && (
-            <button 
-                className={`${touched && 'active'} mobile-hint-button`}
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}>
-                <i className="fa-regular fa-lightbulb"></i>
-            </button>
-          )}
+          <button 
+              className={`${touched && 'active'} mobile-hint-button`}
+              onTouchStart={handleTouchStart}
+              onTouchEnd={handleTouchEnd}
+              onMouseDown={handleTouchStart}
+              onMouseUp={handleTouchEnd}
+              onMouseLeave={handleTouchEnd}
+          >
+              <i className="fa-regular fa-lightbulb"></i>
+          </button>
         </div>
     );
 
